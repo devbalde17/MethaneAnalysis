4f89109bd8fd5855643acce1a5e15cd7b4fb39c2
thierno balde <dev.balde17@gmail.com>
2023-04-19 12:04:36 -0400

    Update test.py

--- test.py
+++ test.py
@@ -31,33 +31,30 @@ top_grouped_data = grouped_data[grouped_data['country'].isin(top_countries)]
 
 # Grouping data by type to get the mean savings and cost
 abatement_grouped = df2.groupby('type').agg({'savings': 'mean', 'cost': 'mean'}).reset_index()
+
 # savings-to-cost ratio
 abatement_grouped['savings_to_cost_ratio'] = abatement_grouped['savings'] / abatement_grouped['cost']
+
 # savings-to-cost ratio in descending order
 sorted_data = abatement_grouped.sort_values(by='savings_to_cost_ratio', ascending=False)
-# print(sorted_data)
-
 
 
 # Create a stacked bar chart for the top 10 countries, broken down by type
-def draw0():
+def emissions_type_wise():
     fig, ax = plt.subplots(figsize=(12, 6))
     top_grouped_data.pivot_table(values='emissions', index='country', columns='type', aggfunc='sum').loc[top_countries].plot(kind='bar', stacked=True, ax=ax)
-    plt.title('Top 10 Countries by Methane Emissions - Type-wise')
+    plt.title('Top 10 Countries by Methane Emissions - Type-wise', color= "green")
     plt.xlabel('Country')
     plt.ylabel('Emissions')
     plt.legend(title='Type', bbox_to_anchor=(1, 1))
     plt.xticks(rotation=45)
-    # plt.show()
-
-
+    
 
 df_oil_gas =  pd.read_csv("oil_gas.csv")
 df_coal = pd.read_csv("coal.csv")
 df_abat = pd.concat([df_oil_gas, df_coal])
-#df_abat.shape
 
-def draw1():
+def emissions_source():
     fig, ax = plt.subplots(figsize=(12, 6))
     top_grouped_data.pivot_table(values='emissions', index='country', columns='type', aggfunc='sum').loc[top_countries].plot(kind='bar', ax=ax)
     plt.title('Top 10 Countries by Methane Emissions - Source-wise')
@@ -65,121 +62,67 @@ def draw1():
     plt.ylabel('Emissions')
     plt.legend(title='Source', bbox_to_anchor=(1, 1))
     plt.xticks(rotation=45)
-    # plt.show()
 
-def draw2():
+def abatement_cost():
     fig, ax = plt.subplots(figsize=(12, 6))
-    sorted_data.plot(x='type', y='savings_to_cost_ratio', kind='bar', ax=ax, legend=None)
+    sorted_data.plot(x='type', y='savings_to_cost_ratio', kind='bar', ax=ax, legend=None, color= "green")
     plt.xlabel('Abatement Type')
     plt.ylabel('Savings-to-Cost Ratio')
     plt.title('Cost Efficiency of Methane Abatement Methods by Source')
     plt.xticks(rotation=75)
-    # plt.show()
 
 
-
-##### Dashboard Stuff #####
-
+# creating dashboard
 
 
 st.set_option('deprecation.showPyplotGlobalUse', False)
 
 
-st.markdown('''<h1 style='text-align: center; color: #7a0099;'>Methane in the Brain</h1><style>
+st.markdown('''<h1 style='text-align: center; color: #028A0F;'>Methane Emissions</h1><style>
 span[data-baseweb="tag"] {
   background-color: purple !important;
 }
 </style>''', unsafe_allow_html=True)   #<----- title)
 
 
-options = ['draw0','draw1', 'draw2', 'draw3', 'draw4']
+options = ['emissions_type_wise', 'emissions_source', 'abatement_cost', 'proportion_total_emissions', 'average_abatement_cost']
 st.sidebar.header("Choose your KPI")
 selecto = st.sidebar.radio("KPI List", options)
 
-def draw3():
+def proportion_total_emissions():
     top_grouped_data_sum = top_grouped_data.groupby('type').agg({'emissions': 'sum'}).reset_index()
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.pie(top_grouped_data_sum['emissions'], labels=top_grouped_data_sum['type'], autopct='%1.1f%%', startangle=90)
     ax.axis('equal')
     plt.title('Proportion of Total Emissions by Type for Top 10 Countries')
-    
+    # plt.show()
 
     
  
-def draw4():
+def average_abatement_cost():
     fig, ax = plt.subplots(figsize=(10, 6))
-    sorted_data.plot(x='type', y='cost', kind='barh', ax=ax, legend=None)
+    sorted_data.plot(x='type', y='cost', kind='barh', ax=ax, legend=None, color= "green")
     plt.xlabel('Average Cost')
     plt.ylabel('Abatement Type')
     plt.title('Average Cost of Methane Abatement Methods by Type')
     # plt.show()
     
-def draw_pie_chart():
-    # Group data by type and sum the emissions for each type
-    type_emissions = df_meth.groupby("type")["emissions"].sum().reset_index()
-
-    # Create the pie chart
-    fig, ax = plt.subplots(figsize=(10, 6))
-    ax.pie(type_emissions["emissions"], labels=type_emissions["type"], autopct="%1.1f%%", startangle=90)
-    ax.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
-    plt.title("Share of Methane Emissions by Type (All Countries)")
-    # plt.show()
-
     
-if selecto == 'draw0':
-    draw0()
+if selecto == 'emissions_type_wise':
+    emissions_type_wise()
     st.pyplot()
     
-elif selecto == 'draw1':
-    draw1()
+elif selecto == 'emissions_source':
+    emissions_source()
     st.pyplot()
-elif selecto == 'draw2':
-    draw2()
+elif selecto == 'abatement_cost':
+    abatement_cost()
     st.pyplot()
-elif selecto == 'draw3':
-    draw3()
+elif selecto == 'proportion_total_emissions':
+    proportion_total_emissions()
     st.pyplot()
-else: # 'draw4'
-    draw4()
+else: #'average_abatement_cost'
+    average_abatement_cost()
     st.pyplot()
 
 
-
-
-
-
-
-
-
-
-# if selecto == 'draw0':
-#     fig_0 = draw0()
-#     st.pyplot(fig_0)
-# elif selecto == 'draw1':
-#     fig_1 = draw1()
-#     st.pyplot(fig_1)
-# elif selecto == 'draw2':
-#     fig_2 = draw2()
-#     st.pyplot(fig_2)
-# elif selecto == 'draw3':
-#     fig_3 = draw3()
-#     st.pyplot(fig_3)
-# else: # 'draw4'
-#     fig_4 = draw4()
-#     st.pyplot(fig_4)
-
-# if selecto == 'draw0':
-#     fig_0 = draw0()
-#     st.pyplot(fig_0)
-# elif selecto == 'draw1':
-#     fig_1 = draw1()
-#     st.pyplot(fig_1)
-# elif selecto == 'draw2':
-#     fig_2 = draw2()
-#     st.pyplot(fig_2)
-# elif selecto == 'draw3':
-#     fig_3 = draw3()
-#     st.pyplot(fig_3)
-# else:
-#     fig_4 = draw4()
-#     st.pyplot(fig_4)
